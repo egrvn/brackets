6be2708495af85954708cad5b5c2652e21b93ba0
frmdth <egrv.nkt@gmail.com>
2023-01-27 20:14:36 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracket = bracketsConfig
+    .reduce((arr, val) => arr
+    .concat(val.join('')), []);
+
+  for (let i = 0; i <= bracket.length; i++) {
+    let template = bracket[i];
+    while (str.includes(template)) {
+      str = str.replace(template, '');
+      i = -1;
+    }
+  }
+  return str.length === 0;
 }
